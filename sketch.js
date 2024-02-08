f7735f2a2fa83e3ad7677594841542177fc63e3f
usung325 <usung325@gmail.com>
2024-02-07 22:23:52 -0500

    randomize but in patterned 4x4's and improved code

--- sketch.js
+++ sketch.js
@@ -1,79 +1,214 @@
+let triSize = 40;   ///////////////////////////////////////////// YOU CAN CHANGE ////////////////////////////////////////////////////////////////////////////////////////////////
+let triAmount = 4;  ///////////////////////////////////////////// DO NOT CHANGE /////////////////////////////////////////////////////////////////////////////////////////////////
+let numRow = 4;     ///////////////////////////////////////////// DO NOT CHANGE /////////////////////////////////////////////////////////////////////////////////////////////////
+
+let my =  Math.sqrt(Math.pow(triSize, 2) - Math.pow(triSize/2, 2));
+
+let canvasX = triSize * (32 * 3);  ///////////////////////////////////////////// YOU CAN CHANGE ////////////////////////////////////////////////////////////////////////////////////////////////
+let canvasY = my * (20 * 3);       ///////////////////////////////////////////// YOU CAN CHANGE ////////////////////////////////////////////////////////////////////////////////////////////////
+
+// let colorList = ['black', 'white']
+let colorArr = [[255,255,0,255],[0,255,255,255],[255,0,0,0],[0,0,255,0]]
+let revColorArr = [[255,255,0,255],[255,0,255,255],[0,0,0,255],[0,255,0,0]]
+
 function setup() {
-  createCanvas(57 * 25, 56 * 15);
-}
+  createCanvas(canvasX, canvasY);
+  noStroke()
 
-function drawTriRow(repNum, incX, incY, row){
-  let offset = 915 
-
-  for (let i = 0; i < repNum; i++){
-      let bnw = ['white', 'black']
-      let color = random(bnw)
-      if (color == 'white'){
-        noStroke()
-        fill(color);
-        triangle(30 - offset + (incX * i) + (row * (incX/2)), // x1
-        75 - offset + (incY * row), // y1
-        58 - offset + (incX * i) + (row * (incX/2)), // x2
-        20 - offset + (incY * row), //y2
-        86 - offset + (incX * i) + (row * (incX/2)), // x3
-        75 - offset + (incY * row)) // y3
-      }
-      else {
-        stroke(0);
-        fill(color);
-        triangle(30 - offset + (incX * i) + (row * (incX/2)), // x1
-        75 - offset + (incY * row), // y1
-        58 - offset + (incX * i) + (row * (incX/2)), // x2
-        20 - offset + (incY * row), //y2
-        86 - offset + (incX * i) + (row * (incX/2)), // x3
-        75 - offset + (incY * row)) // y3
-      }
-      
+  // reassign randomly the pattern arrays
+  for (i = 0; i < 4; i++){
+    for (j = 0; j < 4; j++){
+      colorArr[i][j] = random(255);
     }
-}
-
-function drawTriRowRev(repNum, incX, incY, row){
-  let offset = 915 
-
-  for (let i = 0; i < repNum; i++){
-      let bnw = ['white', 'black']
-      let color = random(bnw)
-      if (color == 'white'){
-        noStroke()
-        fill(color);
-        triangle(58 - offset + (incX * i) + (row * (incX/2)), // x1
-        20 - offset + (incY * row), // y1
-        86 - offset + (incX * i) + (row * (incX/2)), // x2
-        75 - offset + (incY * row), //y2
-        114 - offset + (incX * i) + (row * (incX/2)), // x3
-        20 - offset + (incY * row)) // y3
-      }
-      else {
-        stroke(0);
-        fill(color);
-        triangle(58 - offset + (incX * i) + (row * (incX/2)), // x1
-        20 - offset + (incY * row), // y1
-        86 - offset + (incX * i) + (row * (incX/2)), // x2
-        75 - offset + (incY * row), //y2
-        114 - offset + (incX * i) + (row * (incX/2)), // x3
-        20 - offset + (incY * row)) // y3
-      }
-      
+  }
+  for (i = 0; i < 4; i++){
+    for (j = 0; j < 4; j++){
+      revColorArr[i][j] = random(255);
     }
+  }
 }
+function drawTri(triSize, triAmount, numRow) {
+  for (let i = 0; i < triAmount; i++){
+    for (let j = 0; j < numRow; j++){
+      // let col = random(colorList);
+      // fill(col)
+      fill(colorArr[i][j])
+      triangle(0 + (triSize * i) - ((triSize/2) * j),
+      my + (j * my),
+      triSize/2 + (triSize * i) - ((triSize/2) * j),
+      0 + (j * my), 
+      triSize + (triSize * i) - ((triSize/2) * j), 
+      my + (j * my))
+    }
+  }
+};
 
+function drawTriRev(triSize, triAmount, numRow) {
+  for (let i = 0; i < triAmount; i++){
+    for (let j = 0; j < numRow; j++){
+      // let col = random(colorList);
+      // fill(col)
+      fill(revColorArr[i][j])
+      triangle(triSize/2 + (triSize * i) - ((triSize/2) * j),
+      0 + (j * my),
+      triSize + (triSize * i) - ((triSize/2) * j),
+      my + (j * my), 
+      triSize/2 + triSize + (triSize * i) - ((triSize/2) * j), 
+      0 + (j * my))
+    }
+  }
+};
+
+function patternTriFunc(){
+  //upright triangles
+  for (let i = 0; i < numRow; i++){
+    drawTri(triSize, triAmount, numRow)
+  };
+
+  // translate(-triSize,0);
+
+  //reverse triangles
+  for (let i = 0; i < numRow; i++){
+    drawTriRev(triSize, triAmount, numRow)
+  };
+};
 
 function draw() {
-  background(255);
-  
-  let incX = 57
-  let incY = 56
+  background(0);
+  patternN = 30 ///////////////////////////////////////////// YOU CAN CHANGE ////////////////////////////////////////////////////////////////////////////////////////////////
 
-  let repNum = 90
-  
-  for(let i = 0; i < repNum; i++){
-    drawTriRow(repNum, incX, incY, i)
-    drawTriRowRev(repNum, incX, incY, i)
-  }
+  translate(-triSize * 4, 0);
+
+  for (let i = 0; i < patternN; i++){
+    translate(triSize*4, 0);
+    patternTriFunc();
+  };
+
+  translate(0, my*4);
+
+  for (let i = 0; i < patternN; i++){
+    translate(-triSize*4, 0);
+    patternTriFunc();
+  };
+
+  translate(0, my*4);
+
+  for (let i = 0; i < patternN; i++){
+    translate(triSize*4, 0);
+    patternTriFunc();
+  };
+
+  translate(0, my*4);
+
+  for (let i = 0; i < patternN; i++){
+    translate(-triSize*4, 0);
+    patternTriFunc();
+  };
+  translate(0, my*4);
+
+  for (let i = 0; i < patternN; i++){
+    translate(triSize*4, 0);
+    patternTriFunc();
+  };
+
+  translate(0, my*4);
+
+  for (let i = 0; i < patternN; i++){
+    translate(-triSize*4, 0);
+    patternTriFunc();
+  };
+
+  for (let i = 0; i < patternN; i++){
+    translate(triSize*4, 0);
+    patternTriFunc();
+  };
+
+  translate(0, my*4);
+
+  for (let i = 0; i < patternN; i++){
+    translate(-triSize*4, 0);
+    patternTriFunc();
+  };
+
+  translate(0, my*4);
+
+  for (let i = 0; i < patternN; i++){
+    translate(triSize*4, 0);
+    patternTriFunc();
+  };
+
+  for (let i = 0; i < patternN; i++){
+    translate(-triSize*4, 0);
+    patternTriFunc();
+  };
+  translate(0, my*4);
+
+  for (let i = 0; i < patternN; i++){
+    translate(triSize*4, 0);
+    patternTriFunc();
+  };
+
+  translate(0, my*4);
+
+  for (let i = 0; i < patternN; i++){
+    translate(-triSize*4, 0);
+    patternTriFunc();
+  };
+
+  for (let i = 0; i < patternN; i++){
+    translate(triSize*4, 0);
+    patternTriFunc();
+  };
+
+  translate(0, my*4);
+
+  for (let i = 0; i < patternN; i++){
+    translate(-triSize*4, 0);
+    patternTriFunc();
+  };
+
+  translate(0, my*4);
+
+  for (let i = 0; i < patternN; i++){
+    translate(triSize*4, 0);
+    patternTriFunc();
+  };
+  for (let i = 0; i < patternN; i++){
+    translate(-triSize*4, 0);
+    patternTriFunc();
+  };
+  translate(0, my*4);
+
+  for (let i = 0; i < patternN; i++){
+    translate(triSize*4, 0);
+    patternTriFunc();
+  };
+
+  translate(0, my*4);
+
+  for (let i = 0; i < patternN; i++){
+    translate(-triSize*4, 0);
+    patternTriFunc();
+  };
+
+  for (let i = 0; i < patternN; i++){
+    translate(triSize*4, 0);
+    patternTriFunc();
+  };
+
+  translate(0, my*4);
+
+  for (let i = 0; i < patternN; i++){
+    translate(-triSize*4, 0);
+    patternTriFunc();
+  };
+
+  translate(0, my*4);
+
+  for (let i = 0; i < patternN; i++){
+    translate(triSize*4, 0);
+    patternTriFunc();
+  };
+
   noLoop();
-}
+};
